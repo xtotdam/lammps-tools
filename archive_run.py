e503e617fa88db3797a712065cab957b8e519b46
xtotdam <xtotdam@yandex.ru>
2026-02-26 10:54:08 +0300

    mess with exceptions

--- archive_run.py
+++ archive_run.py
@@ -96,12 +96,6 @@ class LammpsRunner:
     def run_lammps(self):
         try:
             lammps_completed_process = subprocess.run(self.command, shell=True, check=True)
-        except subprocess.CalledProcessError as e:
-            print(f'Run ID = {self.run_id}\n\n{e}')
-            e.add_note(f'Run ID = {self.run_id}')
-            raise
-        except KeyboardInterrupt:
-            pass
         except Exception as e:
             print(f'Run ID = {self.run_id}\n\n{e}')
 
